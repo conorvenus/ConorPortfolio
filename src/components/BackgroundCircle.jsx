65c888bb84652a5a4bdf882f7badab1bd04a5b7e
conorv <70652079+conorvenus@users.noreply.github.com>
2023-08-12 12:29:17 +0100

    Fixed rounded circles displaying over text, making things difficult to click, select, etc

--- src/components/BackgroundCircle.jsx
+++ src/components/BackgroundCircle.jsx
@@ -1,7 +1,7 @@
 export default function BackgroundCircle({ radius, colour, top, left, right, bottom }) {
     return (
         <div 
-        className="rounded-full blur-[150px] opacity-20 absolute" 
+        className="-z-10 rounded-full blur-[150px] opacity-20 absolute" 
         style={{
             width: radius,
             height: radius,
@@ -10,7 +10,6 @@ export default function BackgroundCircle({ radius, colour, top, left, right, bot
             left: left,
             right: right,
             bottom: bottom,
-            zIndex: -1
         }}></div>
     )
 }
