024af9a50141ed7a5f13178b29034ec6341a2858
conorv <70652079+conorvenus@users.noreply.github.com>
2023-08-12 12:42:34 +0100

    Added a ChatMessage component

---
+++ src/components/ChatMessage.jsx
@@ -0,0 +1,10 @@
+import { FireIcon, UserIcon } from "@heroicons/react/24/outline";
+
+export default function ChatMessage({ content, isBot = false }) {
+    return (
+        <div className={`flex items-start gap-2 ${isBot ? 'text-text' : 'text-text-secondary'}`}>
+            {isBot ? <FireIcon className="min-h-6 w-6" /> : <UserIcon className="min-h-6 w-6" />}
+            <p className="flex-1">{content}</p>
+        </div>
+    )
+}
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import { ChatBubbleBottomCenterTextIcon, PaperAirplaneIcon, UserIcon, FaceSmileIcon } from "@heroicons/react/24/outline"
 import { AnimatePresence, motion } from "framer-motion"
+import ChatMessage from "./ChatMessage"
 
 export default function Modal({ visible = false, toggleModal }) {
     return (
@@ -20,15 +21,9 @@ export default function Modal({ visible = false, toggleModal }) {
                         exit={{opacity: 0}}
                         transition={{duration: 0.5, ease: 'easeInOut'}}
                         className="absolute z-20 max-h-[600px] flex flex-col p-4 left-[50%] -translate-x-1/2 mt-16 card max-w-[600px] min-h-[300px] border border-text-secondary rounded-md bg-background bg-opacity-20 backdrop-blur-[25px]">
-                        <main className="flex-1 flex flex-col gap-4 overflow-auto">
-                            <div className="flex items-start gap-2 text-text-secondary">
-                            <UserIcon className="min-h-6 w-6" />
-                            <p className="flex-1">Message content goes here...</p>
-                            </div>
-                            <div className="flex items-start gap-2">
-                            <FaceSmileIcon className="h-6 w-6" />
-                            <p className="flex-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum ratione dolor quam quia consequuntur repudiandae libero quae neque nostrum nobis.</p>
-                            </div>
+                        <main className="flex-1 flex flex-col gap-4 overflow-auto py-4">
+                            <ChatMessage content="Message content goes here..." />
+                            <ChatMessage content="" isBot={true} />
                         </main>
                         <footer className="border border-text-secondary px-2 py-1 rounded-md text-text-secondary flex items-center justify-between">
                             <div className="flex items-center gap-2 flex-1">
