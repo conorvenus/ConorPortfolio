552436f194a296d16fe82ce9bef00d6d9fb1b031
conorv <70652079+conorvenus@users.noreply.github.com>
2023-08-12 17:50:00 +0100

    Removed background circles from changing colour constantly for performance

--- client/src/components/BackgroundCircle.jsx
+++ client/src/components/BackgroundCircle.jsx
@@ -6,28 +6,17 @@ export default function BackgroundCircle({ radius, top, left, right, bottom }) {
         return "#" + Math.floor(Math.random()*16777215).toString(16);
     }
 
-    const [color, setColor] = useState(randomColor())
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-          setColor(randomColor())
-        }, 3000);
-    
-        return () => clearInterval(interval)
-    }, []);
-
     return (
         <motion.div 
         className="-z-10 rounded-full blur-[150px] opacity-20 absolute" 
         style={{
             width: radius,
             height: radius,
-            background: color,
+            background: randomColor(),
             top: top,
             left: left,
             right: right,
-            bottom: bottom,
-            transition: "all 3s ease-in-out"
+            bottom: bottom
         }}></motion.div>
     )
 }
