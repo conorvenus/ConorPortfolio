b52e0027ffdfb500da38807554c549f0d298eb4f
conorv <70652079+conorvenus@users.noreply.github.com>
2023-08-12 18:26:10 +0100

    Added error handling to persist the express server if errors occur

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(express.json())
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+})
+
 const port = process.env.PORT || 5000
 
 app.post("/api/sendMessage", async (req, res) => {
