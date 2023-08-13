6db5a1f65e16211d71a2a782fbe8104543b25451
conorv <70652079+conorvenus@users.noreply.github.com>
2023-08-13 14:18:42 +0100

    Reduced blur on smaller devices for background circles to make them visible

--- client/src/components/BackgroundCircles.jsx
+++ client/src/components/BackgroundCircles.jsx
@@ -3,7 +3,6 @@ import BackgroundCircle from "./BackgroundCircle"
 export default function BackgroundCircles() {
     return (
         <>
-            <BackgroundCircle radius="10vh" top="5vh" left="5vh" />
             <BackgroundCircle radius="30vh" top="-10vh" left="-5vh" />
             <BackgroundCircle radius="40vh" top="15vh" right="-5vh" />
             <BackgroundCircle radius="50vh" top="50vh" left="-5vh" />
